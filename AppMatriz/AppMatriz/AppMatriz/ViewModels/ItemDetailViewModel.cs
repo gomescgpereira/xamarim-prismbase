faa534bbf61c293d166cb76f4765cfad30f86bf1
Carlos <gomescgconsultoria@gmail.com>
2019-07-12 08:03:57 -0300

    Primeiro Commit

---
+++ AppMatriz/AppMatriz/AppMatriz/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AppMatriz.Models;
+
+namespace AppMatriz.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
